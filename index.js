3ae7c81fd2a7f5a7c6f27f0a169d6d296723f6cd
Spatocode <impulseofficial069@gmail.com>
2019-02-22 20:06:55 +0100

    add decrypt algorithm

--- index.js
+++ index.js
@@ -28,7 +28,19 @@ module.exports = () => {
         input.pipe(cipher).pipe(output);
     }
 
-    function decrypt () {
+    function decrypt (password, data) {
+        const algorithm = 'aes-192-cbc'
+        const key = crypto.scryptSync(password, 'salt', 24)
+        const iv = Buffer.alloc(16)
+        
+        const decipher = crypto.createDecipheriv(algorithm, key, iv)
+        const input = fs.createReadStream(data);
+        const output = fs.createWriteStream(data.slice(0, data.length-4));
+
+        input.pipe(decipher).pipe(output);
+    }
+
+    function discard () {
         
     }
 
